7a20ce70bd0ac0362acbb2e28bed68d70ed0957c
Ranjan08385 <moger.ranjan@gmail.com>
2020-11-04 13:08:57 +0530

    Updates

---
+++ precache-manifest.178b87360f297f1530acc5afab31ac61.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edaa2ff13b4df28e1fc26866e93f41ca",
+    "url": "/ranjan/index.html"
+  },
+  {
+    "revision": "2fd7f4a0f9b93c74249e",
+    "url": "/ranjan/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "3a4bde8c2e184dc4f248",
+    "url": "/ranjan/static/css/main.9da987ff.chunk.css"
+  },
+  {
+    "revision": "2fd7f4a0f9b93c74249e",
+    "url": "/ranjan/static/js/2.f6932d47.chunk.js"
+  },
+  {
+    "revision": "30a1a4c051467114c996d3400599fdf7",
+    "url": "/ranjan/static/js/2.f6932d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4bde8c2e184dc4f248",
+    "url": "/ranjan/static/js/main.3b7646e7.chunk.js"
+  },
+  {
+    "revision": "d2c62754110b751ce00f",
+    "url": "/ranjan/static/js/runtime-main.265c2c65.js"
+  },
+  {
+    "revision": "ac9d36621068603001b1ff583631ba7f",
+    "url": "/ranjan/static/media/My_Resume_React.ac9d3662.pdf"
+  },
+  {
+    "revision": "cdfc72fd71839ff05d9102d6b403e378",
+    "url": "/ranjan/static/media/about.cdfc72fd.jpg"
+  },
+  {
+    "revision": "d78e168466b0e0fef25f5a90a0c16d57",
+    "url": "/ranjan/static/media/profile.d78e1684.png"
+  },
+  {
+    "revision": "80b296f1f1933ecf3469cb24d68bc1a2",
+    "url": "/ranjan/static/media/skills.80b296f1.jpg"
+  }
+]);
