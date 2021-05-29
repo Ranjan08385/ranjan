df4a280d70fbe721bb340b918f812ecd30a34f2e
Ranjan08385 <moger.ranjan@gmail.com>
2021-05-29 21:40:48 +0530

    about me section content changes

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,34 +1,50 @@
-import React, {useEffect} from 'react'
-import './About.css'
-import {motion} from 'framer-motion'
-import {about, aboutHead, aboutImag, aboutTextHead, aboutText} from './Animation'
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect } from "react";
+import "./About.css";
+import { motion } from "framer-motion";
+import {
+  about,
+  aboutHead,
+  aboutImag,
+  aboutTextHead,
+  aboutText,
+} from "./Animation";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
-const aboutImg = require('../../images/about.jpg');
+const aboutImg = require("../../images/about.jpg");
 
 function About() {
-    useEffect(() => {
-        Aos.init({ duration: 2000 });
-    }, []);
-    return (
-    
-            <motion.div variants={about} initial="hidden" animate="visible" className="about" id="about">
-                <div data-aos="fade-down" className="about__head">
-                    <motion.h1 variants={aboutHead}>About</motion.h1>
-                </div>
-                <div className="about__imgAndText">
-                    <div data-aos="fade-right" variants={aboutImag} className="about__img">
-                        <img src={aboutImg} alt="" />
-                    </div>
-                    <div data-aos="zoom-in"  className="about__me">
-                        <h2 data-aos="zoom-in" data-aos-delay="100" variants={aboutTextHead}>I'am Ranjan Moger</h2>
-                        <p data-aos="zoom-in" data-aos-delay="200" variants={aboutText}>To work in an organization where I can use my skills and knowledge to deliver value added results that provides me job satisfaction and self development which help me achieve personal as well as organizational goals.</p>
-                    </div>
-                </div>
-            </motion.div>
-      
-    )
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+  return (
+    <motion.div
+      variants={about}
+      initial="hidden"
+      animate="visible"
+      className="about"
+      id="about"
+    >
+      <div data-aos="fade-down" className="about__head">
+        <motion.h1 variants={aboutHead}>About</motion.h1>
+      </div>
+      <div className="about__imgAndText">
+        <div data-aos="fade-right" variants={aboutImag} className="about__img">
+          <img src={aboutImg} alt="" />
+        </div>
+        <div data-aos="zoom-in" className="about__me">
+          <h2 data-aos="zoom-in" data-aos-delay="100" variants={aboutTextHead}>
+            I'am Ranjan Moger
+          </h2>
+          <p data-aos="zoom-in" data-aos-delay="200" variants={aboutText}>
+            Front end developer with 2 plus years of experience in Web and
+            Mobile Applications development using array of technologies like
+            HTML, CSS, Java Script, ReactJs, React Native, JSON, Redux, Nodejs.
+          </p>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
 
-export default About
+export default About;
--- src/pages/skills/Skills.js
+++ src/pages/skills/Skills.js
@@ -1,94 +1,121 @@
-import React, {useEffect} from 'react'
-import 'boxicons'
-import './Skills.css'
-import {motion} from 'framer-motion'
+import React, { useEffect } from "react";
+import "boxicons";
+import "./Skills.css";
+import { motion } from "framer-motion";
 import {
-    skills,
-    skillsHead,
-    skillsLeft,
-    skillsRight,
-    skillsLeftHead,
-    reactJS,
-    reactNative,
-    html,
-    nodeJS
-    } from './Animation'
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+  skills,
+  skillsHead,
+  skillsLeft,
+  skillsRight,
+  skillsLeftHead,
+  reactJS,
+  reactNative,
+  html,
+  nodeJS,
+} from "./Animation";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
-const skillsImg = require('../../images/skills.jpg');
+const skillsImg = require("../../images/skills.jpg");
 
 function Skills() {
-    useEffect(() => {
-        Aos.init({ duration: 2000 });
-    }, []);
-    return (
-      
-            <motion.div variants={skills} initial="hidden" animate="visible" className="skills" id="skills">
-                <div data-aos="fade-down" className="skills__head">
-                    <motion.h1 variants={skillsHead}>Skills</motion.h1>
-                </div>
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+  return (
+    <motion.div
+      variants={skills}
+      initial="hidden"
+      animate="visible"
+      className="skills"
+      id="skills"
+    >
+      <div data-aos="fade-down" className="skills__head">
+        <motion.h1 variants={skillsHead}>Skills</motion.h1>
+      </div>
 
-                <div className="skills__profSkills">
-                    <div data-aos="fade-right" variants={skillsLeft} className="skills__skillSet">
-                        <motion.h2 variants={skillsLeftHead}>Professional Skills</motion.h2>
+      <div className="skills__profSkills">
+        <div
+          data-aos="fade-right"
+          variants={skillsLeft}
+          className="skills__skillSet"
+        >
+          <motion.h2 variants={skillsLeftHead}>Professional Skills</motion.h2>
 
-                        <div data-aos="fade-right" data-aos-delay="100" variants={reactJS} className="skills__section">
-                            <div className="skills__content">
-                                <div className="skilss__iconAndLabel">
-                                    <box-icon color="#4070f4" type='logo' name='react'></box-icon>
-                                    <span>REACT JS</span>
-                                </div>
-                                
-                                <span>80%</span>
-                            </div>
-                            <div className="skills__bars reactJs"></div> 
-                        </div>
+          <div
+            data-aos="fade-right"
+            data-aos-delay="100"
+            variants={reactJS}
+            className="skills__section"
+          >
+            <div className="skills__content">
+              <div className="skilss__iconAndLabel">
+                <box-icon color="#4070f4" type="logo" name="react"></box-icon>
+                <span>REACT JS</span>
+              </div>
 
-                        <div data-aos="fade-right" data-aos-delay="300" variants={reactNative} className="skills__section">
-                            <div className="skills__content">
-                                <div className="skilss__iconAndLabel">
-                                    <box-icon color="#4070f4" type='logo' name='react'></box-icon>
-                                    <span>REACT NATIVE</span>
-                                </div>
-                                
-                                <span>70%</span>
-                            </div>
-                            <div className="skills__bars reactNative"></div> 
-                        </div>
+              <span>75%</span>
+            </div>
+            <div className="skills__bars reactJs"></div>
+          </div>
 
-                        <div data-aos="fade-right" data-aos-delay="600" variants={html} className="skills__section">
-                            <div className="skills__content">
-                                <div className="skilss__iconAndLabel">
-                                    <box-icon color="#4070f4" type='logo' name='html5'></box-icon>
-                                    <span>HTML/CSS</span>
-                                </div>
-                                
-                                <span>75%</span>
-                            </div>
-                            <div className="skills__bars html-css"></div> 
-                        </div>
+          <div
+            data-aos="fade-right"
+            data-aos-delay="300"
+            variants={reactNative}
+            className="skills__section"
+          >
+            <div className="skills__content">
+              <div className="skilss__iconAndLabel">
+                <box-icon color="#4070f4" type="logo" name="react"></box-icon>
+                <span>REACT NATIVE</span>
+              </div>
 
-                        <div data-aos="fade-right" data-aos-delay="900" variants={nodeJS} className="skills__section">
-                            <div className="skills__content">
-                                <div className="skilss__iconAndLabel">
-                                    <box-icon color="#4070f4" type='logo' name='nodejs'></box-icon>
-                                    <span>NODE JS</span>
-                                </div>
-                                
-                                <span>50%</span>
-                            </div>
-                            <div className="skills__bars nodeJs"></div> 
-                        </div>
-                        
-                    </div>
-                    <div data-aos="zoom-in" variants={skillsRight} className="skills__img">
-                        <img src={skillsImg} alt="" />
-                    </div>
-                </div>
-            </motion.div>
+              <span>70%</span>
+            </div>
+            <div className="skills__bars reactNative"></div>
+          </div>
 
-    )
+          <div
+            data-aos="fade-right"
+            data-aos-delay="600"
+            variants={html}
+            className="skills__section"
+          >
+            <div className="skills__content">
+              <div className="skilss__iconAndLabel">
+                <box-icon color="#4070f4" type="logo" name="html5"></box-icon>
+                <span>HTML/CSS</span>
+              </div>
+
+              <span>70%</span>
+            </div>
+            <div className="skills__bars html-css"></div>
+          </div>
+
+          <div
+            data-aos="fade-right"
+            data-aos-delay="900"
+            variants={nodeJS}
+            className="skills__section"
+          >
+            <div className="skills__content">
+              <div className="skilss__iconAndLabel">
+                <box-icon color="#4070f4" type="logo" name="nodejs"></box-icon>
+                <span>JAVASCRIPT</span>
+              </div>
+
+              <span>60%</span>
+            </div>
+            <div className="skills__bars nodeJs"></div>
+          </div>
+        </div>
+        <div data-aos="zoom-in" variants={skillsRight} className="skills__img">
+          <img src={skillsImg} alt="" />
+        </div>
+      </div>
+    </motion.div>
+  );
 }
 
-export default Skills
+export default Skills;
